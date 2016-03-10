66a814094a0bb6c9c2ba147e408658705065aaf4
Uldis Sturms <uldis.sturms@gmail.com>
2016-03-10 22:09:51 +0000

    local-exec instance provisioner and keypair

--- example.tf
+++ example.tf
@@ -5,6 +5,10 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami = "ami-971a65e0"
   instance_type = "t1.micro"
+  key_name = "terraform"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.web.public_ip} > file.txt"
+  }
 }
 
 resource "aws_eip" "ip" {
