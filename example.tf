581d9b45d6084384d6018453b0cedf94d792d54c
Uldis Sturms <uldis.sturms@gmail.com>
2016-03-11 21:21:15 +0000

    security group to ssh into box

--- example.tf
+++ example.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "web" {
   ami = "${lookup(var.amis, var.region)}"
   instance_type = "t1.micro"
   key_name = "terraform"
+  security_groups = ["${aws_security_group.terraform.name}"]
   provisioner "local-exec" {
     command = "echo ${aws_instance.web.public_ip} > file.txt"
   }
@@ -13,4 +14,14 @@ resource "aws_instance" "web" {
 
 resource "aws_eip" "ip" {
   instance = "${aws_instance.web.id}"
+}
+
+resource "aws_security_group" "terraform" {
+  name = "terraform"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
