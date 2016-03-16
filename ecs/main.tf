f9ed51ac06ac9a619f1fc9df8e798cb3726c1e0b
Uldis Sturms <uldis.sturms@gmail.com>
2016-03-16 16:35:05 +0000

    ecs cluster iam role

--- ecs/main.tf
+++ ecs/main.tf
@@ -1,3 +1,22 @@
+resource "aws_iam_role" "ecs_cluster" {
+  name = "EcsCluster"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+}
+EOF
+}
+
 resource "aws_ecs_cluster" "tf_cluster" {
   name = "tf-cluster"
 }
