aff0afe99285236d56c2ac34d8c43bc7350b6dad
Uldis Sturms <uldis.sturms@gmail.com>
2016-03-18 13:53:41 +0000

    provision web instance with nginx, add elb

--- main.tf
+++ main.tf
@@ -6,22 +6,52 @@ resource "aws_instance" "web" {
   ami = "${lookup(var.amis, var.region)}"
   instance_type = "t1.micro"
   key_name = "terraform"
-  security_groups = ["${aws_security_group.terraform.name}"]
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.web.public_ip} > file.txt"
+  security_groups = ["${aws_security_group.internal_web.name}"]
+  tags {
+    Name = "WebHost"
+  }
+  provisioner "remote-exec" {
+    script = "./scripts/install.sh"
+    connection {
+      user = "admin"
+      private_key = "${file("terraform.pem")}"
+    }
   }
 }
 
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.web.id}"
+resource "aws_elb" "web" {
+  name = "web-elb"
+  availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+  instances = ["${aws_instance.web.id}"]
+  tags {
+    Name = "LoadBalancer"
+  }
 }
 
-resource "aws_security_group" "terraform" {
-  name = "terraform"
+resource "aws_security_group" "internal_web" {
+  name = "internal_web"
   ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
---
+++ scripts/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo apt-get update -y
+sudo apt-get install nginx -y
+sudo service nginx start
