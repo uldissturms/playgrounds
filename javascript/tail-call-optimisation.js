801a66eca6a8bdbd5ee96c1703a36905004af838
Uldis Sturms <uldis.sturms@gmail.com>
2016-10-11 17:34:54 +0100

    introduce a generic tail call optimisation (tco) function

---
+++ javascript/tail-call-optimisation.js
@@ -0,0 +1,32 @@
+function tco (f) {
+  var value
+  var active = false
+  var accumulated = []
+
+  return function accumulator () {
+    accumulated.push(arguments)
+
+    if (!active) {
+      active = true
+
+      while (accumulated.length) {
+        value = f.apply(this, accumulated.shift())
+      }
+
+      active = false
+
+      return value
+    }
+  }
+}
+
+var sum = tco(function (x, y) {
+  if (y > 0) {
+    return sum(x + 1, y - 1)
+  } else {
+    return x
+  }
+})
+
+const result = sum(1, 2500000)
+console.log('sum is:', result)
