b5d216190dd5cf1788fda30af46868da578ee069
Uldis Sturms <uldis.sturms@gmail.com>
2017-01-12 21:17:36 +0000

    setup and log into fly to control concourse

---
+++ concourse/install.sh
@@ -0,0 +1,3 @@
+wget "http://localhost:8080/api/v1/cli?arch=amd64&platform=darwin" -O fly
+sudo mv fly /usr/local/bin
+sudo chmod 0755 /usr/local/bin/fly
---
+++ concourse/login.sh
@@ -0,0 +1,1 @@
+fly --target local login --concourse-url http://localhost:8080 --username admin --password admin
