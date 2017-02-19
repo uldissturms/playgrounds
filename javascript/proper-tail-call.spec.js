cebbc7b1299c6c88a09bd09c2df7681a0ca25288
Uldis Sturms <uldis.sturms@gmail.com>
2017-02-19 10:42:44 +0000

    terser sum that is also a PTC

--- javascript/proper-tail-call.spec.js
+++ javascript/proper-tail-call.spec.js
@@ -3,15 +3,23 @@
 const test = require('tape')
 
 // Note: needs use strict and --harmony flag under node 7.5.0
-test('propert tail call optimisation', t => {
+test('propert tail call', t => {
   t.is(sum(1, 100000), 100001)
   t.end()
 })
 
-const sum = function (x, y) {
+test('propert tail call - terse', t => {
+  t.is(sumTerse(1, 100000), 100001)
+  t.end()
+})
+
+const sum = (x, y) => {
   if (y === 0) {
     return x
   }
 
   return sum(x + 1, y - 1)
 }
+
+const sumTerse = (x, y) =>
+  y === 0 ? x : sumTerse(x + 1, y - 1)
