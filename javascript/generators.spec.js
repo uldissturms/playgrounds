787ade5756ec1e798288c16733d1db9a59133028
Uldis Sturms <uldis.sturms@gmail.com>
2017-09-21 23:20:13 +0100

    chore: demo picking up genrator values into array

--- javascript/generators.spec.js
+++ javascript/generators.spec.js
@@ -10,6 +10,14 @@ test('generator returns simple values twice and returns done', t => {
   t.is(iterator.next().done, true)
 })
 
+test('generated values to array', t => {
+  const iterator = simple()
+  const values = Array.from(iterator)
+
+  t.deepEqual(values, [1, 2])
+  t.end()
+})
+
 function * simple () {
   yield 1
   yield 2
