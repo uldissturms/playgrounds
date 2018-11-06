b6ff2a069f7baf3e4615245d02381192d08eec97
Uldis Sturms <uldis.sturms@gmail.com>
2018-11-06 08:19:51 +0000

    chore: add schema built with string & simple resolvers

--- graphql/hello-api/index.js
+++ graphql/hello-api/index.js
@@ -2,11 +2,22 @@ const express = require('express')
 const grapqlHTTP = require('express-graphql')
 const {
   GraphQLSchema,
+  GraphQLID,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  buildSchema
 } = require('graphql')
 
 const app = express()
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    profession: { type: GraphQLString }
+  }
+})
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
@@ -18,10 +29,17 @@ const schema = new GraphQLSchema({
           return '>> hello'
         }
       },
-      world: {
-        type: GraphQLString,
+      user: {
+        type: UserType,
+        args: {
+          id: { type: GraphQLID }
+        },
         resolve () {
-          return '>> world'
+          return {
+            id: '1',
+            name: 'John Doe',
+            profession: 'Sales manager'
+          }
         }
       }
     }
@@ -30,6 +48,37 @@ const schema = new GraphQLSchema({
 
 app.use('/graphql', grapqlHTTP({
   schema,
+  graphiql: true
+}))
+
+const stringSchema = buildSchema(`
+  type User {
+    id: ID!
+    name: String!
+    profession: String
+  }
+  type Query {
+    hello: String,
+    user: User
+  }
+`)
+
+const root = {
+  hello: () => {
+    return 'world'
+  },
+  user: () => {
+    return {
+      id: 1,
+      name: 'John Doe',
+      profession: 'Sales manager'
+    }
+  }
+}
+
+app.use('/graphql-string', grapqlHTTP({
+  schema: stringSchema,
+  rootValue: root,
   graphiql: true
 }))
 
