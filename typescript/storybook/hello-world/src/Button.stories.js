6d3bf8f07153e4f516a5a85651b83c1484583bd7
Uldis Sturms <uldis.sturms@gmail.com>
2018-12-06 20:22:01 +0000

    feat(storybook): hello-world: info addon

--- typescript/storybook/hello-world/src/Button.stories.js
+++ typescript/storybook/hello-world/src/Button.stories.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
 import { Button } from './Button'
 
 storiesOf('button', module)
-  .addWithJSX('green', () => (
+  .addWithJSX('green', withInfo(
+    `consider using this as a primary button`
+  )(() =>
     <Button bg='green'>Hello Green World!</Button>
   ))
-  .addWithJSX('blue', () => (
+  .addWithJSX('blue', () =>
     <Button bg='blue'>Hello Blue World!</Button>
-  ))
+  )
